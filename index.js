e2ddd8b68c9989feb77499aa1ea2a5f5ad610b24
Aviral Makkar <aviralmakkar@outlook.com>
2020-04-30 21:59:48 -0400

    Updated to a Working Navbar and A/M as Logo

---
+++ index.js
@@ -0,0 +1,19 @@
+    /*!
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE)
+    */
+
+   (function ($) {
+    "use strict"; // Start of use strict
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+})(jQuery);
